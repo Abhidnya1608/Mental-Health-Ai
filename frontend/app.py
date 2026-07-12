1fc03d48823e95cfdf577e0aaeb110a44f3f7ea3
Abhidnya1608 <abhidnyamadan04@gmail.com>
2026-07-12 22:59:11 +0530

    added streamlit ui with session state

--- frontend/app.py
+++ frontend/app.py
@@ -7,9 +7,43 @@ st.set_page_config(
 
 st.title("Mental Wellness Ai")
 
-user_input = st.chat_input("Type your messages....")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("Type your message...")
 
 
-if user_input:
-    st.chat_message("user").write(user_input)
-    st.chat_message("assistant").write("Backend is not connected")
+if prompt:
+    st.session_state.messages.append(
+        {
+            'role':'user',
+            'content': prompt
+        }
+    )
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    response = "Hello! I am your Mental Wellness Ai"
+
+    st.session_state.messages.append(
+    {
+        'role':'assistant',
+        'content': response
+    }
+)
+
+    with st.chat_message("assistant"):
+        st.markdown(response)
+
+
+# user_input = st.chat_input("Type ur message")
+
+# if user_input:
+#     st.chat_message("user").write(user_input)
+#     st.chat_message("assistant").write("Hey i am your assistent")
